1e1a409c32b18223ac29ccb3ad600f4561bb80e9
THEMUD <themud2001@hotmail.com>
2020-09-02 17:05:49 +0200

    Initial commit

---
+++ src/components/NavbarCustom.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+function NavbarCustom(props) {
+	return (
+		<Navbar className="navbar-custom justify-content-between" variant="dark">
+			<Navbar.Brand href="#">THEMUD</Navbar.Brand>
+			<Nav>
+				<Nav.Link>
+					<div className="menu-toggler">
+						<svg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'>
+							<path className="menu-hamburger" stroke='#E91E63' strokeLinecap='round' strokeMiterlimit='10' strokeWidth='2' d='M4 7h22M4 15h22M4 23h22'/>
+						</svg>
+					</div>
+				</Nav.Link>
+			</Nav>
+		</Navbar>
+	);
+}
+
+export default NavbarCustom;
