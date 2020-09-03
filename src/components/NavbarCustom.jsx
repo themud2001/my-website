1b813eaf1779512272a4163aa7e4143983639102
THEMUD <themud2001@hotmail.com>
2020-09-03 19:59:37 +0200

    Now when you click on the brand name it scrolls smoothly to the landing page

--- src/components/NavbarCustom.jsx
+++ src/components/NavbarCustom.jsx
@@ -4,17 +4,29 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Hamburger from "./Hamburger";
 
-function NavbarCustom({ isScrolling, onHamburgerClick }) {
-	return (
-		<Navbar fixed="top" className={"navbar-custom justify-content-between" + (isScrolling ? " scrolling" : "")} variant="dark">
-			<Navbar.Brand href="#">THEMUD</Navbar.Brand>
-			<Nav>
-				<Nav.Link onClick={onHamburgerClick}>
-					<Hamburger />
-				</Nav.Link>
-			</Nav>
-		</Navbar>
-	);
+class NavbarCustom extends React.Component {
+	constructor(props) {
+		super(props);
+		this.scrollToPageLanding = this.scrollToPageLanding.bind(this);
+	}
+
+	scrollToPageLanding(e) {
+		e.preventDefault();
+		document.querySelector(".page-landing").scrollIntoView({ behavior: "smooth", block: "start" });
+	}
+
+	render() {
+		return (
+			<Navbar fixed="top" className={"navbar-custom justify-content-between" + (this.props.isScrolling ? " scrolling" : "")} variant="dark">
+				<Navbar.Brand onClick={this.scrollToPageLanding}>THEMUD</Navbar.Brand>
+				<Nav>
+					<Nav.Link onClick={this.props.onHamburgerClick}>
+						<Hamburger />
+					</Nav.Link>
+				</Nav>
+			</Navbar>
+		);
+	}
 }
 
 export default NavbarCustom;
